1fab293daf19f94c77268e38cc315da480ea2d60
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-25 12:00:45 -0600

    Made it wip

---
+++ CSVproject/rubyCSV.rb
@@ -0,0 +1,46 @@
+require 'csv'
+require 'pry'
+accounts = CSV.read('accounts.csv')
+
+#ADDS ITEMS FOR SONIA INTO SEPERATE ARRAY
+
+soniaf = []
+i = 0
+
+CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
+	if accounts [i][0].include? "Sonia"
+		soniaf.push(accounts[i])
+	end
+	i += 1
+end
+
+#ADDS ITEMS FOR PRYIA INTO SEPERATE ARRAY
+
+priyaf = []
+i = 0
+
+CSV.foreach("accounts.csv", {headers: true, return_headers: false}) do |row|
+	if accounts[i][0].include? "Priya"
+		priyaf.push(accounts[i])
+	end
+	i += 1
+end
+
+
+puts "Sonia's file contains: " + soniaf.to_s + soniaf.length.to_s + " Items" + "\n\n"
+puts "Priya's file contains: " + priyaf.to_s + priyaf.length.to_s + " Items" + "\n\n"
+binding.pry
+puts "done"
+
+
+
+
+
+
+
+
+
+
+
+
+
