9c3c5a951c979699f9fe296b630b8c2c627fc656
Spencer Gerdes <skgerdes@yahoo.com>
2017-01-31 15:21:26 -0600

    Tests functions.

---
+++ CSVproject/tests.rb
@@ -0,0 +1,19 @@
+require 'CSVclasses.rb'
+
+#check if priya total sums up
+
+def testSum
+
+
+
+end
+
+
+
+def correctness
+	if actual === expected
+		puts "Test passed"
+	else
+		puts "Test failed, expected #{expected""
+	end
+end
