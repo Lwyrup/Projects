d75945ed05bd826033b52460be7a6cf4077e68b2
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-05 14:15:31 -0600

    Switched from rubyrps to webrps to make life easy

--- rpsproject/webrps.rb
+++ rpsproject/webrps.rb
@@ -8,3 +8,29 @@ def areValid(weapon)
 		return false
 	end
 end
+
+def validator(p1_weapon, p2_weapon)
+	if areValid(p1_weapon) and areValid(p2_weapon)
+		decider(p1_weapon, p2_weapon)
+	else
+		return @message = "Invalid choice\ntry again."
+	end
+end
+
+def decider(weapon1, weapon2)
+	if weapon1 != weapon2
+		whoWins(weapon1, weapon2)
+	else 
+		return @message = "Valid but same."
+	end
+end
+
+def whoWins(weapon1, weapon2)
+	if (weapon1 == "rock" && weapon2 == "scissors" ||
+		weapon1 == "paper" && weapon2 == "rock" ||
+		weapon1 == "scissors" && weapon2 == "paper")
+		return @message = "player 1 wins", $scoreOne += 50
+	else
+		return @message = "player 2 wins"
+	end
+end
