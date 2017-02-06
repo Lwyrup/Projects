64fe66db18a9902df3bfd84083caf03e98cd5972
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-05 18:50:34 -0600

    Changed over to knight theme

--- rpsproject/webrps.rb
+++ rpsproject/webrps.rb
@@ -13,7 +13,7 @@ def validator(p1_weapon, p2_weapon)
 	if areValid(p1_weapon) and areValid(p2_weapon)
 		decider(p1_weapon, p2_weapon)
 	else
-		return @message = "Invalid choice\ntry again."
+		return @message = "Sire! Thy choice is illegal!"
 	end
 end
 
@@ -21,7 +21,7 @@ def decider(weapon1, weapon2)
 	if weapon1 != weapon2
 		whoWins(weapon1, weapon2)
 	else 
-		return @message = "Valid but same."
+		return @message = "Tis' a tie! Sire!"
 	end
 end
 
@@ -29,8 +29,23 @@ def whoWins(weapon1, weapon2)
 	if (weapon1 == "rock" && weapon2 == "scissors" ||
 		weapon1 == "paper" && weapon2 == "rock" ||
 		weapon1 == "scissors" && weapon2 == "paper")
-		return @message = "player 1 wins", $scoreOne += 50
+		return @message = "Knight_1 wins", $scoreOne += 1
 	else
-		return @message = "player 2 wins"
+		return @message = "Knight_2 wins", $scoreTwo += 1
 	end
 end
+
+
+def keepPlaying
+	if $scoreOne == 3
+		redirect("/game?win=Knight_1")
+	elsif $scoreTwo == 3
+		redirect("/game?win=Knight_2")
+	end
+end
+
+
+
+
+
+
