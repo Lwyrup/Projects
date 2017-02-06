865c9d080cf86031e36277783ba35647ca96e4ec
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-05 18:51:12 -0600

    Added game over get response

--- rpsproject/controlers.rb
+++ rpsproject/controlers.rb
@@ -2,29 +2,27 @@ require_relative 'webrps.rb'
 require 'sinatra'
 
 $scoreOne = 0
+$scoreTwo = 0
 
 get("/home"){
 	@message = params["message"]
-	$scoreOne
 	erb :home
 }
-
 
 post("/home"){
-	@var = params
 	@p1_weapon = params["weapon_one"]
 	@p2_weapon = params["weapon_two"]
-
 	validator(@p1_weapon, @p2_weapon)
-	$scoreOne += 1
-	#compare and return winner
-
-
+	keepPlaying
 	erb :home
 }
 
-
-
+get("/game"){
+	@winner = params["win"]
+	$scoreOne = 0
+	$scoreTwo = 0
+	erb :gameover
+}
 
 
 
