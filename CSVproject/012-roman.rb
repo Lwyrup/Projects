d6f28af9bdf2aae23b605ef42b2d0e6e6a0a83a3
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-07 09:43:12 -0600

    Toying with hashes

---
+++ CSVproject/012-roman.rb
@@ -0,0 +1,19 @@
+#converstion table
+
+m = 1000
+d = 500
+c = 100
+l = 50	
+x = 10
+v = 5
+i = 1
+
+
+input = 363
+ccount = 0
+#loop like the change problem
+
+while(input > 99) 
+	input -= 100
+	ccount + 1
+end
