15e5e8f3527c502e5c03e040234160d2ac59ff12
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-14 08:56:27 -0600

    Notes

--- waldo/tony_stark.php
+++ waldo/tony_stark.php
@@ -1,12 +1,12 @@
 <?php
 
-$f = fopen('locations.csv', 'r');
-# somevar = fread
-# fclose
-# somevar == location send via $_POST???
-# return result client-side
+// $f = fopen('locations.csv', 'r');
+// # somevar = fread
+// # fclose
+// # somevar == location send via $_POST???
+// # return result client-side
 
-
+#try to have the comparisons here and return a console.log
 
 
 
