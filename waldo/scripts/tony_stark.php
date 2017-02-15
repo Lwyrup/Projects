71c5f85df63fd4dfbef22a51a3bf2187d0047c91
Spencer Gerdes <skgerdes@yahoo.com>
2017-02-15 16:19:02 -0600

    Moved Tony over to the scripts folder for organization purposes

---
+++ waldo/scripts/tony_stark.php
@@ -0,0 +1,44 @@
+<?php
+$file = '../locations.csv';
+
+$f = fopen($file, 'r');
+$text = fread($f, filesize($file));
+fclose($f);
+
+$locationOfWaldo = str_getcsv($text);
+echo checkCoords($locationOfWaldo);
+
+function checkCoords($loW){
+	if (withinRange($loW, 'x') && withinRange($loW, 'y')){
+		return "√ WALDO FOUND!";
+	}
+	else{
+		return "Try again";
+	};
+};
+
+function withinRange($loW, $xORy){
+	$axis = isItXorY($xORy);
+	if ($_POST[$xORy."pos"] > $loW[$axis] - 10 && $_POST[$xORy."pos"] < $loW[$axis] + 10){
+		return true;
+	}
+	else{
+		return false;
+	};
+};
+
+function isItXorY($xy){
+	if ($xy == "x"){
+		return 0;
+	}
+	else if ($xy == "y"){
+		return 1;
+	};
+};
+
+// echo("\nsent with post: ".$_POST["xpos"]." ".$_POST["ypos"]);
+
+//All php files print/echo as a response default is outside tags
+//print/echo is how to bridge a servers response to the js
+?>
+
