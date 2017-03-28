c5a3de86f3afa9bf219bdc1b0fa9f2af1fd89f63
Spencer Gerdes <skgerdes@yahoo.com>
2017-03-28 11:13:58 -0500

    Postgres

--- todo-list/db/schema.rb
+++ todo-list/db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20170322152637) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "todos", force: :cascade do |t|
     t.string   "name"
     t.integer  "user_id"
